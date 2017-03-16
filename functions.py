b9de5ba77bd43d849ab8597126c8f7d6297bf888
VolkerGast <volker.gast@uni-jena.de>
2017-03-16 23:30:37 +0100

    added functions.py

---
+++ functions.py
@@ -0,0 +1,30 @@
+
+node_type_map = {
+    'p': 'paragraph',
+    's': 'segment',
+    'sp': 'speaker',
+    'a': 'anno',
+    't': 'token'}
+
+edge_type_map = {
+    'a': 'anno',
+    'o': 'order'}
+
+map_type_map = {
+    's': 'segment',
+    'p': 'paragraph',
+    'sp': 'speaker'}
+
+meta_data_keys = {
+    'file_name',
+    'info',
+    'version',
+    'anno_makros',
+    'conf',
+    'search_makros',
+    'file_settings',
+    'annotators',
+    'tagset',
+    'media'}
+
+
